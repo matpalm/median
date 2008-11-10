37e5687663b893dbac9961e0842123cb5bc0e8f2
mat <matthew.kelcey@gmail.com>
2008-11-10 20:07:59 +1100

    multi process erlang impl

---
+++ controller.erl
@@ -0,0 +1,137 @@
+-module(controller).
+-export([init/1]).
+%-compile(export_all).
+
+init(Files) ->
+    put(workers, spawn_workers(Files)),
+    NumberElements = lists:sum(workers_request(length)),
+    M = median(NumberElements),
+    io:format("~w\n",[M]),
+    M.
+
+spawn_workers(Files) ->
+    register(controller, self()),
+    lists:map(	      
+      fun(File) -> spawn(worker,init,[File]) end,
+      Files
+     ).
+
+median(NumberElements) ->
+    nth_order_stat(round(NumberElements/2)).
+
+nth_order_stat(Target_order_stat) ->
+    prune_workers_without_data(),
+    { Min, Max } = workers_min_max(),
+    % TODO do we ever exit on Max clause?
+    if 
+	(Min == Max) or (Target_order_stat == 1) ->
+	    Min;    
+	true ->
+	    partition_on_pivot(Target_order_stat)	
+    end.
+    
+partition_on_pivot(Target_order_stat) ->
+    Pivot = select_pivot(),
+    Num_less_than = lists:sum(workers_request({less_than,Pivot})),
+    Pivot_order_stat = Num_less_than + 1, 
+
+    if 
+	Pivot_order_stat == Target_order_stat ->
+	    broadcast_to_workers(shutdown),
+	    Pivot;
+
+	Pivot_order_stat == 1 ->
+	    rotate(),
+	    nth_order_stat(Target_order_stat);
+
+	Pivot_order_stat < Target_order_stat ->
+	    broadcast_to_workers({filter_gt_eq, Pivot}),
+	    rotate(),
+	    Adjusted_target_order_stat = Target_order_stat - Num_less_than, 
+	    nth_order_stat(Adjusted_target_order_stat);
+
+	true -> % Pivot_order_stat > Target_order_stat
+	    broadcast_to_workers({filter_lt, Pivot}),
+	    nth_order_stat(Target_order_stat)
+    end.
+
+select_pivot() ->
+    hd(get(workers)) ! { request, pivot},
+    receive
+	{ pivot, Pivot} ->
+	    Pivot
+    end.   
+
+rotate() -> 
+    [H|T] = get(workers),
+    H ! rotate,
+    put(workers, T++[H]).
+
+prune_workers_without_data() ->
+    broadcast_to_workers({request, have_data}),
+    Alive_workers = receive_alive_workers(length(get(workers)), []),
+    put(workers, Alive_workers).
+
+receive_alive_workers(0, Alive_workers) ->
+    lists:reverse(Alive_workers);
+
+receive_alive_workers(Num_remaining, Workers) ->
+    receive
+	{ alive, WorkerPid } ->	    
+	    receive_alive_workers(Num_remaining-1, [WorkerPid|Workers]);
+	dead ->
+	    receive_alive_workers(Num_remaining-1, Workers)
+    end.
+
+workers_min_max() ->
+    MinMaxs = workers_request(min_max), % list of min/maxs [ {2,3}, {0,4}, {4,9} ... ]
+    lists:foldl(
+      fun({NextMin,NextMax},{Min,Max}) -> { min(NextMin,Min), max(NextMax,Max) } end,
+      hd(MinMaxs),
+      MinMaxs
+     ).
+	
+min(A,B) ->
+    case A < B of
+	true -> A;
+	false -> B
+    end.
+
+max(A,B) ->
+    case A > B of
+	true -> A;
+	false -> B
+    end.    
+	    
+workers_request(Type) ->
+    % request 
+    lists:map(
+      fun(P) -> P ! { request, Type } end,
+      get(workers)
+     ),
+    % receive responses
+    lists:map(
+      fun(_) -> 
+	      receive
+		  { Type, N } -> 
+		      N
+	      after 60000 ->
+		      io:format("missed a response\n")
+	      end
+	end,
+      get(workers)
+     ).
+
+broadcast_to_workers(Msg) ->
+    lists:map(
+      fun(P) -> P ! Msg end,
+      get(workers)  
+     ).	    
+    
+
+			 
+			 
+			  
+
+
+
---
+++ worker.erl
@@ -0,0 +1,74 @@
+-module(worker).
+-export([init/1]).
+%-compile(export_all).
+
+init(File) ->
+    List = parse_file:parse(File),
+    loop(List).
+
+loop(List) ->
+%    io:format("~w entering loop with ~w\n",[self(),List]),
+    receive
+	{ request, length } ->
+	    controller ! { length, length(List) },
+	    loop(List);
+
+	{request, have_data} ->
+	    case length(List) == 0 of
+		true ->
+		    controller ! dead,
+		    no_more_data;
+		false ->
+		    controller ! { alive, self() },
+		    loop(List)
+	    end;
+
+	{ request, min_max } ->
+	    controller ! { min_max, { lists:min(List), lists:max(List) } },
+	    loop(List);
+
+	{ request, pivot } ->
+	    controller ! { pivot, hd(List) },
+	    loop(List);
+
+	{ request, {less_than,N} = LtMsg } ->
+	    Num_less_than = number_less_than(List, N),
+	    controller ! { LtMsg, Num_less_than },
+	    loop(List);
+
+	rotate ->
+	    loop(rotate(List));
+
+	{ filter_gt_eq, N } ->
+	    loop(lists:filter(fun(E) -> E >= N end, List));
+
+	{ filter_lt, N } ->
+	    loop(lists:filter(fun(E) -> E < N end, List));
+
+	shutdown ->
+	    shutdown;
+
+	Msg ->
+	    io:format("~w received unexpected Msg ~w\n",[self(),Msg]),
+	    fail_dog
+
+    
+    after 1500 ->
+	    io:format("~w timeout\n",[self()])
+
+    end.
+
+	    
+rotate([H|T]) -> 
+    T ++ [H].
+
+number_less_than(List,N) -> 
+    lists:foldl(fun(X,Acc) ->
+			case X < N  of
+			    true ->
+				Acc+1;
+			    false ->
+				Acc
+			end
+		end,
+		0, List).    
